f503943a928a4649797283c4876fbe18023f4ac4
saisree1234567 <150107861+saisree1234567@users.noreply.github.com>
2024-07-02 13:51:55 -0400

    sai-home-page home.vue file is added and modified router file for home and login routes

---
+++ src/views/home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref,onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const user = ref(null);
+onMounted(async () => {
+  user.value = JSON.parse(localStorage.getItem("user"));
+  console.log("user",user.value)
+    if(!user.value) {
+    router.push({ name: "login" });
+  }
+});
+</script>
+<template>
+    <h2>Home page</h2>
+</template>
